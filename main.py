21655960d00592e9fe9ac6c7e136dde2e6992fcf
Pimwatchara <pimwatchara62@gmail.com>
2025-05-20 15:02:59 +0700

    แก้โค้ด
    Signed-off-by: Pimwatchara <pimwatchara62@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from vision_aruco import VisionSystem
 from vision_manual import VisionManual
 
 if __name__ == "__main__":
-    camera = VisionSystem()
-    # camera = VisionManual()
+    # camera = VisionSystem()
+    camera = VisionManual()
     camera.run()
